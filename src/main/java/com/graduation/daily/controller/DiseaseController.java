af6bdf21c525dccacea6950c85dcc59f118714a0
jini_11 <jieun21124@naver.com>
2022-11-08 22:51:11 +0900

    질환 상세 페이지

--- src/main/java/com/graduation/daily/controller/DiseaseController.java
+++ src/main/java/com/graduation/daily/controller/DiseaseController.java
@@ -17,4 +17,28 @@ public class DiseaseController {
     public String diseaseTurtleNeck() {
         return "/disease/disease_turtleneck";
     }
+
+    @GetMapping("/disease_rcs")
+    public String disease_rcs() {return "disease/disease_rcs";}
+
+    @GetMapping("/disease_hand")
+    public String disease_hand(){return "disease/disease_hand";}
+
+    @GetMapping("/disease_halluxValgus")
+    public String disease_halluxV(){return "disease/disease_halluxValgus";}
+
+    @GetMapping("/disease_bowlegs")
+    public String disease_bowlegs(){return "disease/disease_bowlegs";}
+
+    @GetMapping("/disease_scoliosis")
+    public String disease_scoliosis() {return "disease/disease_scoliosis";}
+
+    @GetMapping("/disease_frozenshoulder")
+    public String disease_frozenshoulder() {return "disease/disease_frozenshoulder";}
+
+    @GetMapping("/disease_hivd")
+    public String disease_hivd() {return "disease/disease_hivd";}
+
+    @GetMapping("/disease_roundshoulder")
+    public String disease_roundshoulder() {return "disease/disease_roundshoulder";}
 }
