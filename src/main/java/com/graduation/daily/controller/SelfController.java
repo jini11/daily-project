3bb0847905f4e5ff317388475f16986583e1abb9
jini_11 <jieun21124@naver.com>
2022-11-08 23:30:20 +0900

    자가진단 페이지 추가

--- src/main/java/com/graduation/daily/controller/SelfController.java
+++ src/main/java/com/graduation/daily/controller/SelfController.java
@@ -12,4 +12,31 @@ public class SelfController {
     public String index() {
         return "self/index";
     }
+
+    @GetMapping("/self_turtleneck")
+    public String self_turtleneck() {return "self/self_turtleneck";}
+
+    @GetMapping("/self_scoliosis")
+    public String self_scoliosis() {return "self/self_scoliosis";}
+
+    @GetMapping("/self_frozen_shoulder")
+    public String self_frozen_shoulder(){return "self/self_frozen_shoulder";}
+
+    @GetMapping("/self_disk")
+    public String self_disk(){return "self/self_disk";}
+
+    @GetMapping("/self_rcs")
+    public String self_rcs(){return "self/self_rcs";}
+
+    @GetMapping("/self_hand")
+    public String self_hand(){return "self/self_hand";}
+
+    @GetMapping("/self_roundshoulder")
+    public String self_roundshoulder(){return "self/self_roundshoulder";}
+
+    @GetMapping("/self_bowlegs")
+    public String self_bowlegs(){return "self/self_bowlegs";}
+
+    @GetMapping("/self_halluxValgus")
+    public String self_halluxValgus(){return "self/self_halluxvalgus";}
 }
